8e29fc3e811718f59654970923df765454dcbf61
Paul Hutchin <paul.hutchin@flextrade.com>
2020-05-13 11:37:59 +0100

    Deleted build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-if [[ ${TRAVIS_PULL_REQUEST} = 'false' ]];
-then
-    gpg --version
-	  gpg --import flexUK-private-key.gpg
-    mvn clean deploy -P sign-artifacts --settings ./settings.xml;
-else
-    mvn clean verify;
-fi
